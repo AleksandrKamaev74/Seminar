4b24231e3a828f964a1aab9b63ca85011ac6ef3f
Aleksandr Kamaev <al.kamaev@yandex.ru>
2022-08-24 22:10:08 +0300

    Решение задачи №64

---
+++ Task64/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значения M и N. 
+//Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+int M = InputNumbers("Введите число M=");
+int N = InputNumbers("Введите число N=");
+Console.WriteLine(PrintNumbers(M, N));
+
+//Вывод натуральных чисел от M до N
+String PrintNumbers(int M, int N)
+{
+    string result = String.Empty; //пустая строка
+    for (int i = M; i <= N; i = i + 1)
+    {
+        result = result + $"{i} ";
+    }
+    return result;
+}
+
+//Ввод целых чисел M,N
+int InputNumbers(string input)
+{
+    Console.Write(input);//ввод строк
+    int output = Convert.ToInt32(Console.ReadLine());//преобразование строк в числа
+    return output;
+}
